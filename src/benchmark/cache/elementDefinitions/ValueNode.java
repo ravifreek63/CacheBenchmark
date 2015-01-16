59f0dc2e57293aead337c2f8d3ac9f7a5648718f
ravitandon <ravifreek63@gmail.com>
2015-01-16 17:36:48 -0500

    size of ValueNode increased

--- src/benchmark/cache/elementDefinitions/ValueNode.java
+++ src/benchmark/cache/elementDefinitions/ValueNode.java
@@ -2,8 +2,10 @@ package benchmark.cache.elementDefinitions;
 
 public class ValueNode {
 	private int _id;
+	private char[] array;
 	public ValueNode(int id){
 		_id = id;
+		array = new char[100];
 	}
 	public int getId(){
 		return _id;
